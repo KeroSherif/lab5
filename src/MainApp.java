6442961de84dfa7bbce1702d9dc9aed535160792
danah19112005-lang <danah19112005@gmail.com>
2025-10-25 22:04:19 +0300

    Create MainApp.java

---
+++ src/MainApp.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author DANAH
+ */
+public class MainApp extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
