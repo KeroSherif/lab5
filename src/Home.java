22d43f441464dbf3609464bf61650b71165b6862
danah19112005-lang <danah19112005@gmail.com>
2025-10-25 21:30:40 +0300

    Create Home.java

---
+++ src/Home.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author DANAH
+ */
+public class HomePanel extends JPanel {
+    private MainApp mainApp;
+
+    public HomePanel(MainApp mainApp) {
+        this.mainApp = mainApp;
+        setLayout(new BorderLayout());
+
+       
